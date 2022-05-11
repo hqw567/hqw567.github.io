2f2c232c1e24b7584f4615a81b2e6c64d37f5cf8
Your Name <you@example.com>
2022-05-11 16:18:04 +0800

    :pencil: update content

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@ info=$1
 if ["$info" = ""]; then
   info=":pencil: update content"
 fi
+export HEXO_ALGOLIA_INDEXING_KEY=fc7ef947951ad1901007b65c3b29a7cd
 hexo algolia
 git add -A
 git commit -m "$info"
