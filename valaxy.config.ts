8ee5f2b1172a7d0c222bbf7cc4313286e7a2c352
unknown <hqw6@qq.com>
2023-02-20 16:38:32 +0800

    :art: 更新

--- valaxy.config.ts
+++ valaxy.config.ts
@@ -1,51 +1,65 @@
-import { defineValaxyConfig } from 'valaxy'
-import type { UserThemeConfig } from 'valaxy-theme-yun'
+import { defineSiteConfig } from "valaxy";
 
-// add icons what you will need
-const safelist = [
-  'i-ri-home-line',
-]
-
-/**
- * User Config
- */
-export default defineValaxyConfig<UserThemeConfig>({
-  // site config see site.config.ts
-
-  theme: 'yun',
-
-  themeConfig: {
-    banner: {
-      enable: true,
-      title: '云游君的小站',
-      cloud: {
-        enable: true,
-      },
+export default defineSiteConfig({
+  search: {
+    enable: false,
+    // 设置类型为 Fuse
+    type: "fuse",
+  },
+  url: "http://simimi.cn/valaxy-test2/",
+  lang: "zh-CN",
+  title: "思米米(SIMIMI)",
+  subtitle: "All at sea.",
+  author: {
+    avatar: "https://q1.qlogo.cn/g?b=qq&nk=79099400&s=640",
+    status: {
+      emoji: "😣",
     },
-
-    pages: [
+    name: "Herway",
+    email: "hqw567@gmail.com",
+  },
+  favicon: "/favicon.svg",
+  description: "山有木兮木有枝，心悦君兮君不知。",
+  mediumZoom: { enable: true },
+  social: [
+    // {
+    //   name: "RSS",
+    //   link: "/atom.xml",
+    //   icon: "i-ri-rss-line",
+    //   color: "orange",
+    // },
+    {
+      name: "GitHub",
+      link: "https://github.com/hqw567",
+      icon: "i-ri-github-line",
+      color: "#6e5494",
+    },
+  ],
+  comment: {
+    enable: true,
+  },
+  sponsor: {
+    enable: true,
+    title: "我很可爱，请给我钱！",
+    methods: [
       {
-        name: '我的小伙伴们',
-        url: '/links/',
-        icon: 'i-ri-genderless-line',
-        color: 'dodgerblue',
+        name: "支付宝",
+        url: "/images/pay/zfb.jpg",
+        color: "#00A3EE",
+        icon: "i-ri-alipay-line",
       },
       {
-        name: '喜欢的女孩子',
-        url: '/girls/',
-        icon: 'i-ri-women-line',
-        color: 'hotpink',
+        name: "QQ 支付",
+        url: "/images/pay/qq.png",
+        color: "#12B7F5",
+        icon: "i-ri-qq-line",
+      },
+      {
+        name: "微信支付",
+        url: "/images/pay/wx.png",
+        color: "#2DC100",
+        icon: "i-ri-wechat-pay-line",
       },
     ],
-
-    footer: {
-      since: 2016,
-      beian: {
-        enable: true,
-        icp: '苏ICP备17038157号',
-      },
-    },
   },
-
-  unocss: { safelist },
-})
+});
