4a605fff7ee25396e939db1d83bb90ff5e78ed0e
Herway <hqw6@qq.com>
2023-02-23 10:50:50 +0800

    Update: 2023-02-23 10:50:50 - author: Herway

--- push.sh
+++ push.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-# 从命令行参数中获取提交信息，如果没有则使用默认值
-author=$(git config user.username || git config user.name)
+# 设置全局 Git 用户名和邮箱
+git config --global user.name "Herway"
+git config --global user.email "hqw6@qq.com"
+
+# 获取当前 Git 用户名和提交时间
+author=$(git config user.name)
 commit_time=$(date "+%Y-%m-%d %H:%M:%S")
+
+# 设置默认的提交信息，包含时间戳和作者信息
 default_info="Update: $commit_time - author: $author"
+
+# 如果命令行参数提供了自定义的提交信息，则使用自定义信息，否则使用默认信息
 info=${1:-"$default_info"}
 
-
-# 如果提交信息为空，输出错误信息并退出
+# 如果提交信息为空，则输出错误消息并退出
 if [[ -z $info ]]; then
   echo "Error: Please provide a commit message."
   exit 1
 fi
 
-# 提交本地代码更改
+# 添加所有更改并提交
 git add -A
 git commit -m "$info"
+
+# 推送到远程仓库的主分支
 git push origin main
