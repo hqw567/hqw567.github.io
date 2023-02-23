ba33567b12acdffd65a59a10d79d6b6c0e33e725
unknown <hqw6@qq.com>
2023-02-23 10:38:23 +0800

    Update: 2023-02-23 10:38:23 - author:

--- push.sh
+++ push.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # 从命令行参数中获取提交信息，如果没有则使用默认值
-info=${1:-":pencil: update content"}
+default_info="Update: $(date +%Y-%m-%d\ %H:%M:%S) - author: $(git config user.name)"
+info=${1:-"$default_info"}
+
 
 # 如果提交信息为空，输出错误信息并退出
 if [[ -z $info ]]; then
