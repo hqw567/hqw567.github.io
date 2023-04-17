70b07df6626dad7518e1a9cdab9c69a2fa8ca463
Herway <hqw6@qq.com>
2023-04-17 14:15:59 +0800

    Update: 2023-04-17 14:15:59 - author: Herway

--- components/UpyunLogo.vue
+++ components/UpyunLogo.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 const upyun = {
   name: "又拍云",
-  url: "https://www.upyun.com/?utm_source=lianmeng&utm_medium=hqw567",
+  url: "https://www.upyun.com/?utm_source=lianmeng&utm_medium=referral",
   logo: "http://img.simimi.cn/img/league/upyun-logo.png",
 };
 </script>
