546a5f55b012bb70288b053fa6fb279331ca0d76
Herway <hqw6@qq.com>
2025-07-05 17:01:56 +0800

    Update: 2025-07-05 17:01:56 - author: Herway

--- setup/main.ts
+++ setup/main.ts
@@ -4,8 +4,8 @@ import { install as installGtag } from "./gtag";
 
 export default defineAppSetup((ctx) => {
 	// 只在生产环境执行分析工具
-	if (import.meta.env.PROD) {
-		installGtag(ctx);
-		installClarity(ctx);
-	}
+	// if (import.meta.env.PROD) {
+	// 	installGtag(ctx);
+	// 	installClarity(ctx);
+	// }
 });
